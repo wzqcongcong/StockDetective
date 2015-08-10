5047d241540d3cfc8b46af3554c0db48f4c39f6a
wzqcongcong <wzqcongcong@sina.com>
2015-08-10 19:33:37 +0800

    init Today Extension

---
+++ ExtensionToday/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  ExtensionToday
+//
+//  Created by GoKu on 8/10/15.
+//  Copyright (c) 2015 GoKuStudio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TodayViewController : NSViewController
+
+@end
